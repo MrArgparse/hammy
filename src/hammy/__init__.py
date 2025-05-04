a1427efbd7033183e90fd5b73f397079b407023e
MrArgparse <156494427+MrArgparse@users.noreply.github.com>
2025-05-03 21:05:35 -0400

    Add files via upload
    fixed change function name

--- src/hammy/__init__.py
+++ src/hammy/__init__.py
@@ -1,1 +1,1 @@
-from .__main__ import find_images, organize_pics, resize_pics, upload_pics, format_links, get_progress
+from .__main__ import find_images, organize_pics, resize_pics, upload_image, format_links, get_progress
