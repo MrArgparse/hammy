2d89dd9c2d1eb53c4034a5a25b112f2db1addc4d
MrArgparse <156494427+MrArgparse@users.noreply.github.com>
2025-05-09 21:32:10 -0400

    version 0.2
    Added prompt for API key, changed readme, code formatting with ruff, improves progress logic, resize logic, added types-pyperclip as dep and removed unused deps

--- src/hammy/__main__.py
+++ src/hammy/__main__.py
@@ -19,53 +19,78 @@ import pyperclip
 import sys
 import tomlkit
 
-EXTENSIONS = ['.bmp', '.gif', '.jpg', '.jpeg', '.png', '.webp']
+EXTENSIONS = frozenset(['.bmp', '.gif', '.jpg', '.jpeg', '.png', '.webp'])
 PLATFORMDIRS = platformdirs.PlatformDirs(appname='hammy', appauthor=False)
 CONFIG_FOLDER = PLATFORMDIRS.user_config_path
 DEFAULT_CONFIGURATION_PATH = CONFIG_FOLDER / 'hammy_config.toml'
 DEFAULT_ENCODING = 'utf-8'
-logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='[%X]', handlers=[RichHandler()])
+logging.basicConfig(
+	level=logging.INFO, format='%(message)s', datefmt='[%X]', handlers=[RichHandler()]
+)
 
-class DefaultConfig(msgspec.Struct):
+
+class DefaultConfig(msgspec.Struct, kw_only=True):
 	api_key: str = ''
 	download_path: Path = CONFIG_FOLDER / 'downloads'
 	resize_path: Path = CONFIG_FOLDER / 'resize'
 	txt_path: Path = CONFIG_FOLDER / 'txt'
 
+
 def parse_fappy() -> argparse.ArgumentParser:
 	parser = argparse.ArgumentParser(prog='hammy')
 	parser.add_argument('source', nargs='+', help='File, folder or url (recursive)')
-	parser.add_argument('--clip', '-c', action='store_true', help='Sets the resulting links in the clipboard')
-	parser.add_argument('--single', '-s', action='store_true', help='Outputs the links on a single line')
-	parser.add_argument('--width', '-w', type=int, default=None, help='Resize images to desired width value in pixels')
-	parser.add_argument('--format', default='d', choices=['b', 'd', 'h', 'i', 'm', 't', 'u'], help='Selects desired link formats')
-	parser.add_argument('--txt', '-t', action='store_true', default=False, help='Outputs links to a text file')
-
+	parser.add_argument(
+		'--clip',
+		'-c',
+		action='store_true',
+		help='Sets the resulting links in the clipboard',
+	)
+	parser.add_argument(
+		'--single', '-s', action='store_true', help='Outputs the links on a single line'
+	)
+	parser.add_argument(
+		'--width',
+		'-w',
+		type=int,
+		default=None,
+		help='Resize images to desired width value in pixels',
+	)
+	parser.add_argument(
+		'--format',
+		default='d',
+		choices=['b', 'd', 'h', 'i', 'm', 't', 'u'],
+		help='Selects desired link formats',
+	)
+	parser.add_argument(
+		'--txt',
+		'-t',
+		action='store_true',
+		default=False,
+		help='Outputs links to a text file',
+	)
 	return parser
 
+
 def encode_hook(obj: Path | str) -> str:
-
 	if isinstance(obj, Path):
-
 		return str(obj)
 
-	return obj  
+	return obj
+
 
 def decode_hook(type_: Type[Path], value: Path | str) -> Path | str:
-
 	if type_ is Path and isinstance(value, str):
-
 		return Path(value)
 
 	return value
 
-def get_config_path(path: Path | None = None) -> Path:
 
+def get_config_path(path: Path | None = None) -> Path:
 	if path is None:
-		
 		return DEFAULT_CONFIGURATION_PATH
 
 	return Path(path).resolve()
+
 
 def load_config(path: Path | None = None) -> DefaultConfig:
 	path = get_config_path(path)
@@ -74,6 +99,7 @@ def load_config(path: Path | None = None) -> DefaultConfig:
 		data = fp.read()
 
 	return msgspec.toml.decode(data, type=DefaultConfig, dec_hook=decode_hook)
+
 
 def save_config(configuration: DefaultConfig, path: Path | None = None) -> None:
 	path = get_config_path(path)
@@ -85,6 +111,7 @@ def save_config(configuration: DefaultConfig, path: Path | None = None) -> None:
 
 	logging.info(f'New default config saved in: {DEFAULT_CONFIGURATION_PATH}')
 
+
 def load_or_create_config(path: Path | None = None) -> DefaultConfig:
 	path = get_config_path(path)
 
@@ -92,58 +119,77 @@ def load_or_create_config(path: Path | None = None) -> DefaultConfig:
 		logging.info(f'Previous config found in: {DEFAULT_CONFIGURATION_PATH}')
 
 	try:
-
 		return load_config(path)
 
 	except FileNotFoundError:
-
 		pass
 
 	configuration = DefaultConfig()
 	save_config(configuration, path)
-	
+
 	return configuration
+
 
 CONFIG = load_or_create_config()
 
-def ensure_directories_exist(config: DefaultConfig) -> None:
+if not CONFIG.api_key:
+	try:
+		CONFIG.api_key = input('Enter api key: ')
 
+		if CONFIG.api_key:
+			save_config(CONFIG)
+
+		else:
+			raise ValueError('Input field was empty')
+
+	except ValueError as e:
+		logging.error(f'{type(e).__name__}: {e}')
+		logging.shutdown()
+		sys.exit(1)
+
+
+def ensure_directories_exist(config: DefaultConfig) -> None:
 	for field in config.__struct_fields__:
 		value = getattr(config, field)
 
 		if isinstance(value, Path):
-
 			if not value.suffix:
 				value.mkdir(parents=True, exist_ok=True)
 
+
 ensure_directories_exist(CONFIG)
 
+
 def create_retry() -> requests.Session:
-	retry_strategy = Retry(total=2, status_forcelist=[429, 500, 502, 503, 504], allowed_methods=['HEAD', 'GET', 'OPTIONS'])
-	adapter = HTTPAdapter(max_retries = retry_strategy)
+	retry_strategy = Retry(
+		total=2,
+		status_forcelist=[429, 500, 502, 503, 504],
+		allowed_methods=['HEAD', 'GET', 'OPTIONS'],
+	)
+	adapter = HTTPAdapter(max_retries=retry_strategy)
 	http = requests.Session()
 	http.mount('https://', adapter)
 
 	return http
 
+
 def find_images(arg: Path) -> list[Path]:
 	image_files = []
 
 	for root, dirs, filenames in os.walk(arg):
- 
 		for filename in filenames:
 			full_path = Path(root) / filename
- 
+
 			if full_path.suffix.lower() in EXTENSIONS:
 				image_files.append(full_path)
 
 	return sorted(image_files)
 
+
 def organize_pics(filenames: list[Path], resize: int | None = None) -> list[Path]:
 	pics = []
 
 	for arg in filenames:
-
 		if arg.is_dir():
 			pics.extend(find_images(arg))
 
@@ -151,35 +197,35 @@ def organize_pics(filenames: list[Path], resize: int | None = None) -> list[Path
 			pics.append(arg)
 
 	for index, pic in enumerate(pics):
-
 		if resize is not None:
 			pics[index] = resize_pics(pics[index], resize)
 
 		while pics[index].stat().st_size > 7600000:
-			logging.warning(f'Image size is too big! Current Size: {pics[index].stat().st_size}')
+			logging.warning(
+				f'Image size is too big! Current Size: {pics[index].stat().st_size}'
+			)
 			pics[index] = resize_pics(pics[index])
 
 			if pics[index].stat().st_size >= 7600000:
-
 				continue
 
 	pics.sort()
 
 	return pics
 
+
 def check_width(new_width: int, width: int) -> int:
-
 	try:
-
 		new_width = int(input(f'Current width: {width}{os.linesep}Enter new width: '))
 
 		if new_width < 1 or new_width >= width:
-
 			raise ValueError
 
 	except ValueError:
-			logging.error('Invalid input. Enter a valid integer greater than 0 and lower than the current width.')
-	
+		logging.error(
+			'Invalid input. Enter a valid integer greater than 0 and lower than the current width.'
+		)
+
 	return new_width
 
 
@@ -201,6 +247,7 @@ def resize_pics(pic: Path, resize: int | None = None) -> Path:
 
 	return fname
 
+
 def download_image(url: str) -> Path:
 	r = requests.get(url)
 	file_name = PurePosixPath(url).name
@@ -208,8 +255,9 @@ def download_image(url: str) -> Path:
 
 	with open(dl_path, 'wb') as f:
 		f.write(r.content)
-	
+
 	return dl_path
+
 
 def upload_image(image_path: Path) -> tuple[str, str]:
 	url = 'https://hamster.is/api/1/upload'
@@ -229,14 +277,24 @@ def upload_image(image_path: Path) -> tuple[str, str]:
 
 	link = response.json()['image']['url']
 	image_id = response.json()['image']['id_encoded']
-	
+
 	return link, image_id
 
+
 def get_progress(process_type: str) -> tuple[Progress, Progress]:
-	description_progress =  Progress(TextColumn(f'{process_type} ' + 'file: {task.fields[extra]}'), auto_refresh=False)
-	progress_bar = Progress(TextColumn('[progress.percentage]{task.percentage:>3.0f}%'), BarColumn(), MofNCompleteColumn(), auto_refresh=False)
+	description_progress = Progress(
+		TextColumn(f'{process_type} ' + 'file: {task.fields[extra]}'),
+		auto_refresh=False,
+	)
+	progress_bar = Progress(
+		TextColumn('[progress.percentage]{task.percentage:>3.0f}%'),
+		BarColumn(),
+		MofNCompleteColumn(),
+		auto_refresh=False,
+	)
 
 	return description_progress, progress_bar
+
 
 def save_txt(path_name: Path, text_string: str) -> None:
 	path_name.parent.mkdir(parents=True, exist_ok=True)
@@ -244,17 +302,18 @@ def save_txt(path_name: Path, text_string: str) -> None:
 	with open(path_name, 'a', encoding='utf-8', newline='\n') as txt_file:
 		txt_file.write(text_string)
 
+
 def get_out() -> Path:
 	date: str = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
 	path_name = CONFIG.txt_path / f'links-{date}.txt'
 
 	return path_name
 
+
 def format_links(link_format: list[str], link: str, image_id: str) -> str:
 	result = ''
 
 	match link_format:
-
 		case 'b':
 			result = f'[img]{link}[/img]'
 
@@ -262,21 +321,22 @@ def format_links(link_format: list[str], link: str, image_id: str) -> str:
 			result = link
 
 		case 'h':
-			result = f'[url=https://hamster.is/image/{image_id}][img]{change_url_suffix(link, 'th')}[/img][/url]'
+			result = f'[url=https://hamster.is/image/{image_id}][img]{change_url_suffix(link, "th")}[/img][/url]'
 
 		case 'i':
 			result = f'[imgnm]{link}[/imgnm]'
 
 		case 'm':
-			result = f'{change_url_suffix(link, '.md')}'
+			result = f'{change_url_suffix(link, ".md")}'
 
 		case 't':
-			result = f'{change_url_suffix(link, '.th')}'
-		
+			result = f'{change_url_suffix(link, ".th")}'
+
 		case 'h':
-			result = f'[url=https://hamster.is/image/{image_id}][img]{change_url_suffix(link, 'md')}[/img][/url]'
+			result = f'[url=https://hamster.is/image/{image_id}][img]{change_url_suffix(link, "md")}[/img][/url]'
 
 	return result
+
 
 def change_url_suffix(url: str, new_suffix: str) -> str:
 	parts = urlparse(url)
@@ -286,16 +346,19 @@ def change_url_suffix(url: str, new_suffix: str) -> str:
 
 	return urlunparse(new_parts)
 
+
 def is_url(s: str) -> bool:
 	parsed = urlparse(s)
 
-	return parsed.scheme in ("http", "https") and bool(parsed.netloc)
+	return parsed.scheme in ('http', 'https') and bool(parsed.netloc)
+
 
 def separate_sources(sources: list[str]) -> tuple[list[str], list[Path]]:
 	urls = [s for s in sources if is_url(s)]
 	files_or_dirs = [Path(s) for s in sources if not is_url(s)]
 
 	return urls, files_or_dirs
+
 
 def main() -> None:
 	parser = parse_fappy()
@@ -313,18 +376,14 @@ def main() -> None:
 		if sys.stdout.isatty():
 			description_progress0, progress_bar0 = get_progress('Downloading')
 			group0 = Group(description_progress0, progress_bar0)
+			task0 = progress_bar0.add_task('Task 0', total=total)
+			task1 = description_progress0.add_task('Task 1', total=total, extra=urls[0])
 
 		else:
 			group0 = None
 
 		with Live(group0):
-
-			if group0:
-				task0= progress_bar0.add_task('Task 0', total=total)
-				task1 = description_progress0.add_task('Task 1', total=total, extra=urls[0])
-
 			for u in urls:
-			
 				try:
 					dl = download_image(u)
 
@@ -337,7 +396,9 @@ def main() -> None:
 
 				if group0:
 					progress_bar0.update(task0, advance=1, refresh=True)
-					description_progress0.update(task1, advance=1, refresh=True, extra=u)
+					description_progress0.update(
+						task1, advance=1, refresh=True, extra=u
+					)
 
 		pics.extend(downloads)
 
@@ -352,23 +413,27 @@ def main() -> None:
 	if sys.stdout.isatty():
 		description_progress1, progress_bar1 = get_progress('Uploading')
 		group1 = Group(description_progress1, progress_bar1)
+		task3 = progress_bar1.add_task('Task 3', total=total)
+		task4 = description_progress1.add_task(
+			'Task 4', total=total, extra=fname
+		)  # put in a func?
 
 	else:
 		group1 = None
 
 	with Live(group1):
-
-		if group1:
-			task3 = progress_bar1.add_task('Task 3', total=total)
-			task4 = description_progress1.add_task('Task 4', total=total, extra=fname)
-
 		for pic in pics:
 			fname = pic.name
 
 			try:
 				link, image_id = upload_image(pic)
 
-			except (AttributeError, KeyError, requests.exceptions.HTTPError, ValueError) as e:
+			except (
+				AttributeError,
+				KeyError,
+				requests.exceptions.HTTPError,
+				ValueError,
+			) as e:
 				logging.error(f'{type(e).__name__}: {e}')
 
 				continue
@@ -377,7 +442,9 @@ def main() -> None:
 
 			if group1:
 				progress_bar1.update(task3, advance=1, refresh=True)
-				description_progress1.update(task4, advance=1, refresh=True, extra=fname)
+				description_progress1.update(
+					task4, advance=1, refresh=True, extra=fname
+				)
 
 	if args.single:
 		text_string = ''.join(result_string)
@@ -393,5 +460,6 @@ def main() -> None:
 
 	Console().print(text_string, markup=False)
 
-if __name__  ==  '__main__':
+
+if __name__ == '__main__':
 	main()
